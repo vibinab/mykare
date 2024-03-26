ea10feee778d8d44fabc70ae4030b1534c33744f
jomon jose <jzjomon@gmail.com>
2024-03-26 13:43:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Login from './components/Login'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
